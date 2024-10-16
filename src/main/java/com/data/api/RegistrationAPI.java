ebdc43ce1fedb58ac555705e44c9a4789d075d11
Ruth Blackman <rkjb410@gmail.com>
2024-10-16 19:21:03 +0100

    added endpoints for registration

--- src/main/java/com/data/api/RegistrationAPI.java
+++ src/main/java/com/data/api/RegistrationAPI.java
@@ -5,9 +5,9 @@ import com.data.repository.RegistrationRepository;
 import com.data.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/registrations")
@@ -20,6 +20,55 @@ public class RegistrationAPI {
     public ResponseEntity<?> getAllRegistrations() {
         Iterable<Registration> registrations = repo.findAll();
         return ResponseEntity.ok(registrations);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getRegistration(@PathVariable("id") long id) {
+        Optional<Registration> registration = repo.findById(id);
+
+        if(registration.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(registration);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addRegistration(@RequestBody Registration registration) {
+        if(registration.getCustomer() == null || registration.getEvent() == null || registration.getDate() == null || registration.getNotes() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Registration savedRegistration = repo.save(registration);
+        return ResponseEntity.ok(savedRegistration);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putRegistration(@RequestBody Registration registration, @PathVariable("id") long id) {
+        Optional<Registration> existingRegistration = repo.findById(id);
+
+        if(existingRegistration.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Registration updatedRegistration = existingRegistration.get();
+        updatedRegistration.setEvent(registration.getEvent());
+        updatedRegistration.setCustomer(registration.getCustomer());
+
+        repo.save(updatedRegistration);
+        return ResponseEntity.ok(updatedRegistration);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteRegistration(@PathVariable("id") long id) {
+        Optional<Registration> registration = repo.findById(id);
+
+        if(registration.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        repo.delete(registration.get());
+        return ResponseEntity.ok().build();
     }
 
 
